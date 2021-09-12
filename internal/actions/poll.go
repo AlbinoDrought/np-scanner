a467b233457190453f9f596e2f9b2161adb23686
Sean <sean@albinodrought.com>
2021-09-12 12:21:00 -0700

    feat: poll errors do not prevent other polling attmepts

--- internal/actions/poll.go
+++ internal/actions/poll.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"go.albinodrought.com/neptunes-pride/internal/matchstore"
@@ -32,6 +33,18 @@ func (err PollError) Error() string {
 	return fmt.Sprintf("%v: %+v | [gameNumber=%v] [playerUID=%v] [playerAlias=%v]", err.Message, err.Base, err.GameNumber, err.PlayerUID, err.PlayerAlias)
 }
 
+type MultiError struct {
+	Errors []error
+}
+
+func (err MultiError) Error() string {
+	messages := make([]string, len(err.Errors))
+	for i, innerError := range err.Errors {
+		messages[i] = innerError.Error()
+	}
+	return strings.Join(messages, "|")
+}
+
 func PollMatch(ctx context.Context, db matchstore.MatchStore, client npapi.NeptunesPrideClient, gameNumber string, pollOptions *PollOptions) error {
 	if pollOptions == nil {
 		pollOptions = &DefaultPollOptions
@@ -51,6 +64,8 @@ func PollMatch(ctx context.Context, db matchstore.MatchStore, client npapi.Neptu
 		return nil
 	}
 
+	pollErrors := []error{}
+
 	for i, config := range match.PlayerCreds {
 		if !pollOptions.Force && time.Since(config.LastPoll) < pollOptions.MinTimePassed {
 			log.Printf("recently polled game %v user %v \"%v\" on %v", gameNumber, config.PlayerUID, config.PlayerAlias, config.LastPoll)
@@ -63,24 +78,26 @@ func PollMatch(ctx context.Context, db matchstore.MatchStore, client npapi.Neptu
 		})
 
 		if err != nil {
-			return PollError{
+			pollErrors = append(pollErrors, PollError{
 				Base:        err,
 				GameNumber:  gameNumber,
 				PlayerUID:   config.PlayerUID,
 				PlayerAlias: config.PlayerAlias,
 				Message:     "failed fetching remote state",
-			}
+			})
+			continue
 		}
 
 		err = db.SaveSnapshot(gameNumber, resp)
 		if err != nil {
-			return PollError{
+			pollErrors = append(pollErrors, PollError{
 				Base:        err,
 				GameNumber:  gameNumber,
 				PlayerUID:   config.PlayerUID,
 				PlayerAlias: config.PlayerAlias,
 				Message:     "failed saving snapshot",
-			}
+			})
+			continue
 		}
 
 		config.LastPoll = time.Now()
@@ -93,21 +110,39 @@ func PollMatch(ctx context.Context, db matchstore.MatchStore, client npapi.Neptu
 	match.LastPoll = time.Now()
 	err = db.SaveMatch(match)
 	if err != nil {
-		return PollError{
+		pollErrors = append(pollErrors, PollError{
 			Base:       err,
 			GameNumber: gameNumber,
 			Message:    "failed saving game",
-		}
+		})
+	}
+
+	if len(pollErrors) == 1 {
+		return pollErrors[0]
+	}
+
+	if len(pollErrors) > 0 {
+		return MultiError{pollErrors}
 	}
 
 	return nil
 }
 
 func PollMatches(ctx context.Context, db matchstore.MatchStore, client npapi.NeptunesPrideClient, gameNumbers []string, pollOptions *PollOptions) error {
+	pollErrors := []error{}
+
 	for _, gameNumber := range gameNumbers {
 		if err := PollMatch(ctx, db, client, gameNumber, pollOptions); err != nil {
-			return err
+			pollErrors = append(pollErrors, err)
 		}
+	}
+
+	if len(pollErrors) == 1 {
+		return pollErrors[0]
+	}
+
+	if len(pollErrors) > 0 {
+		return MultiError{pollErrors}
 	}
 
 	return nil
