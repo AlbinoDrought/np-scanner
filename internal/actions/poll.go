a842129f2a3e4adc43a9806ff2dd938798c77f6f
Sean <sean@albinodrought.com>
2021-09-11 23:51:51 -0700

    feat: move poll logic outside of command

---
+++ internal/actions/poll.go
@@ -0,0 +1,122 @@
+package actions
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.albinodrought.com/neptunes-pride/internal/matchstore"
+	"go.albinodrought.com/neptunes-pride/internal/npapi"
+)
+
+type PollOptions struct {
+	Force         bool
+	MinTimePassed time.Duration
+}
+
+var DefaultPollOptions = PollOptions{
+	Force:         false,
+	MinTimePassed: 15 * time.Minute,
+}
+
+type PollError struct {
+	Base        error
+	GameNumber  string
+	PlayerUID   int
+	PlayerAlias string
+	Message     string
+}
+
+func (err PollError) Error() string {
+	return fmt.Sprintf("%v: %+v | [gameNumber=%v] [playerUID=%v] [playerAlias=%v]", err.Message, err.Base, err.GameNumber, err.PlayerUID, err.PlayerAlias)
+}
+
+func PollMatch(ctx context.Context, db matchstore.MatchStore, client npapi.NeptunesPrideClient, gameNumber string, pollOptions *PollOptions) error {
+	if pollOptions == nil {
+		pollOptions = &DefaultPollOptions
+	}
+
+	match, err := db.FindMatchOrFail(gameNumber)
+	if err != nil {
+		return PollError{
+			Base:       err,
+			GameNumber: gameNumber,
+			Message:    "failed finding match",
+		}
+	}
+
+	if len(match.PlayerCreds) == 0 {
+		log.Printf("match %v has no credentials", gameNumber)
+		return nil
+	}
+
+	for i, config := range match.PlayerCreds {
+		if !pollOptions.Force && time.Since(config.LastPoll) < pollOptions.MinTimePassed {
+			log.Printf("recently polled game %v user %v \"%v\" on %v", gameNumber, config.PlayerUID, config.PlayerAlias, config.LastPoll)
+			continue
+		}
+
+		resp, err := client.State(ctx, &npapi.Request{
+			GameNumber: gameNumber,
+			APIKey:     config.APIKey,
+		})
+
+		if err != nil {
+			return PollError{
+				Base:        err,
+				GameNumber:  gameNumber,
+				PlayerUID:   config.PlayerUID,
+				PlayerAlias: config.PlayerAlias,
+				Message:     "failed fetching remote state",
+			}
+		}
+
+		err = db.SaveSnapshot(gameNumber, resp)
+		if err != nil {
+			return PollError{
+				Base:        err,
+				GameNumber:  gameNumber,
+				PlayerUID:   config.PlayerUID,
+				PlayerAlias: config.PlayerAlias,
+				Message:     "failed saving snapshot",
+			}
+		}
+
+		config.LastPoll = time.Now()
+		match.PlayerCreds[i] = config
+
+		log.Printf("retrieved state for game %v user %v \"%v\"", gameNumber, config.PlayerUID, config.PlayerAlias)
+	}
+
+	match.LastPoll = time.Now()
+	err = db.SaveMatch(match)
+	if err != nil {
+		return PollError{
+			Base:       err,
+			GameNumber: gameNumber,
+			Message:    "failed saving game",
+		}
+	}
+
+	return nil
+}
+
+func PollMatches(ctx context.Context, db matchstore.MatchStore, client npapi.NeptunesPrideClient, gameNumbers []string, pollOptions *PollOptions) error {
+	for _, gameNumber := range gameNumbers {
+		if err := PollMatch(ctx, db, client, gameNumber, pollOptions); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func PollAllMatches(ctx context.Context, db matchstore.MatchStore, client npapi.NeptunesPrideClient, pollOptions *PollOptions) error {
+	matches, err := db.Matches()
+	if err != nil {
+		return err
+	}
+
+	return PollMatches(ctx, db, client, matches, pollOptions)
+}
--- internal/cmd/poll.go
+++ internal/cmd/poll.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/spf13/cobra"
-	"go.albinodrought.com/neptunes-pride/internal/npapi"
+	"go.albinodrought.com/neptunes-pride/internal/actions"
 )
 
 var (
@@ -15,7 +15,7 @@ var (
 )
 
 var pollCmd = &cobra.Command{
-	Use:   "poll [...game numbers]",
+	Use:   "poll [...game numbers, or \"all\"]",
 	Short: "Poll games",
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
@@ -24,58 +24,24 @@ var pollCmd = &cobra.Command{
 			log.Fatal("failed to open DB", err)
 		}
 
-		if len(args) == 1 && args[0] == "all" {
-			args, err = db.Matches()
-			if err != nil {
-				log.Fatal("failed getting all matches", err)
-			}
-		}
-
 		client := openClient()
 
-		for _, gameNumber := range args {
-			match, err := db.FindMatchOrFail(gameNumber)
-			if err != nil {
-				log.Fatal("failed finding match", gameNumber, err)
-			}
-
-			if len(match.PlayerCreds) == 0 {
-				log.Printf("match %v has no credentials", gameNumber)
-				continue
-			}
-
-			for i, config := range match.PlayerCreds {
-				if !pollCmdForce && time.Since(config.LastPoll) < pollCmdMinTimePassed {
-					log.Printf("recently polled game %v user %v \"%v\" on %v", gameNumber, config.PlayerUID, config.PlayerAlias, config.LastPoll)
-					continue
-				}
-
-				resp, err := client.State(context.Background(), &npapi.Request{
-					GameNumber: gameNumber,
-					APIKey:     config.APIKey,
-				})
-
-				if err != nil {
-					log.Fatalf("failed fetching state for game %v user %v \"%v\": %v", gameNumber, config.PlayerUID, config.PlayerAlias, err)
-				}
-
-				err = db.SaveSnapshot(gameNumber, resp)
-				if err != nil {
-					log.Fatalf("failed saving snapshot for game %v user %v \"%v\": %v", gameNumber, config.PlayerUID, config.PlayerAlias, err)
-				}
-
-				config.LastPoll = time.Now()
-				match.PlayerCreds[i] = config
-
-				log.Printf("retrieved state for game %v user %v \"%v\"", gameNumber, config.PlayerUID, config.PlayerAlias)
-			}
-
-			match.LastPoll = time.Now()
-			err = db.SaveMatch(match)
-			if err != nil {
-				log.Fatalf("failed saving game %v: %v", gameNumber, err)
-			}
+		pollOptions := &actions.PollOptions{
+			Force:         pollCmdForce,
+			MinTimePassed: pollCmdMinTimePassed,
 		}
+
+		if len(args) == 1 && args[0] == "all" {
+			err = actions.PollAllMatches(context.Background(), db, client, pollOptions)
+		} else {
+			err = actions.PollMatches(context.Background(), db, client, args, pollOptions)
+		}
+
+		if err != nil {
+			log.Fatal("polling failed", err)
+		}
+
+		log.Println("polled")
 	},
 }
 
